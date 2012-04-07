9cba986247dd374d4883c019c28d091e93f7a6a0
Andrew Taeoalii <ulu_5@yahoo.com>
2012-04-06 18:18:01 -0600

    Initial Commit

---
+++ ATRayTracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ATRayTracer
+//
+//  Created by Andrew Taeoalii on 4/6/12.
+//  Copyright (c) 2012 Apple Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
